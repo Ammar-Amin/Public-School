035d4e625c25b400ae9591c3f1c1b498bbb53d2f
Ammar-Amin <ammaramin5253@gmail.com>
2024-07-31 20:40:02 +0530

    faculty page & gallery completed

--- src/pages/Faculty.jsx
+++ src/pages/Faculty.jsx
@@ -1,12 +1,136 @@
 import React from 'react'
 import Gallery from './Gallery'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Faculty() {
+
+    let facultyData = [
+        {
+            "name": "Dr. Emily Johnson",
+            "photo": "https://stoneridgesoftware.com/wp-content/uploads/2022/10/Emily-Johnson-Profile.jpg",
+            "position": "Head of Science Department",
+            "qualifications": "PhD in Physics, MSc in Astrophysics",
+            "experience": "15 years of teaching experience in high school and university levels"
+        },
+        {
+            "name": "Mr. David Smith",
+            "photo": "https://th.bing.com/th/id/OIP.cHeY3iSk74JNiDjzE_TFwwAAAA?rs=1&pid=ImgDetMain",
+            "position": "Mathematics Teacher",
+            "qualifications": "MSc in Mathematics, BEd",
+            "experience": "10 years of teaching experience in secondary education"
+        },
+        {
+            "name": "Ms. Sarah Williams",
+            "photo": "https://th.bing.com/th/id/OIP.vjVgeeFHG50jfBGOFmm_mQAAAA?rs=1&pid=ImgDetMain",
+            "position": "English Teacher",
+            "qualifications": "MA in English Literature, PGCE",
+            "experience": "12 years of teaching experience in middle and high school"
+        },
+        {
+            "name": "Mr. Michael Brown",
+            "photo": "https://ohsobserver.com/wp-content/uploads/2017/09/DrHendrickson-800x1200.jpg",
+            "position": "History Teacher",
+            "qualifications": "MA in History, BEd",
+            "experience": "8 years of teaching experience in secondary schools"
+        },
+        {
+            "name": "Dr. Linda Davis",
+            "photo": "https://th.bing.com/th/id/OIP.nDRapGf2CjMb3PPGlOZ8mwHaJQ?rs=1&pid=ImgDetMain",
+            "position": "Biology Teacher",
+            "qualifications": "PhD in Biology, MSc in Environmental Science",
+            "experience": "13 years of teaching experience and research in biological sciences"
+        },
+        {
+            "name": "Mr. James Wilson",
+            "photo": "https://th.bing.com/th/id/OIP.ojC9EAuxw_7fvj3vOcg-oQHaFQ?rs=1&pid=ImgDetMain",
+            "position": "Physical Education Teacher",
+            "qualifications": "BSc in Physical Education, Certified Fitness Trainer",
+            "experience": "9 years of teaching experience sports coaching"
+        },
+        {
+            "name": "Ms. Jennifer Martinez",
+            "photo": "https://th.bing.com/th/id/OIP.GYwlYdd55dRvQZfFX87wvgHaIS?rs=1&pid=ImgDetMain",
+            "position": "Art Teacher",
+            "qualifications": "MFA in Fine Arts, BEd",
+            "experience": "11 years of teaching experience in art and design"
+        },
+        {
+            "name": "Dr. Robert Clark",
+            "photo": "https://th.bing.com/th/id/OIP.nj0oAQNpmXLam2Ec5_GWdQAAAA?w=400&h=400&rs=1&pid=ImgDetMain",
+            "position": "Chemistry Teacher",
+            "qualifications": "PhD in Chemical Engineering",
+            "experience": "14 years of teaching experience in high school and college-level chemistry"
+        }
+    ]
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    }
+
     return (
         <div className='w-full min-h-screen bg-purple-900 text-white'>
-            <Gallery />
-            <div className='h-[600px] flex items-center justify-center'>
-                <span className='text-5xl'>Faculty</span>
+            <div className='max-w-7xl mx-auto p-3 md:p-10 '>
+                <div className='w-full min-h-screen lg:text-lg flex flex-col-reverse items-center justify-center md:flex-row gap-4 lg:gap-8'>
+                    <div className='md:w-1/2'>
+                        <h1 className='text-4xl lg:text-5xl xl:text-7xl font-semibold mb-8'>Discover Our <div className='hidden md:block'></div> Expert Faculty
+                        </h1>
+                        <p>Meet the skilled professionals dedicated to advancing education in Pune. Explore their qualifications and experience.</p>
+                    </div>
+                    <div className='md:w-1/2'>
+                        <img src='https://liberalarts.oregonstate.edu/sites/liberalarts.oregonstate.edu/files/sps_group_small.jpg' alt='faculty Image' />
+                    </div>
+                </div>
+                <div className='mb-10 w-[90%] mx-auto md:w-full min-h-[600px]'>
+                    <h1 className='text-4xl lg:text-5xl xl:text-7xl font-semibold mb-8'>Qualifications and experience.
+                    </h1>
+                    <Slider {...settings}>
+                        {
+                            facultyData.map((item, i) => <div key={i}>
+                                <div key={i} className='m-3 xl:m-6 min-h-[350px] p-4 bg-blue-600 border-2 border-white rounded-3xl'>
+                                    <div className='flex flex-col justify-evenly items-center'>
+                                        <img src={item.photo} alt='img'
+                                            className='w-[70px] h-[70px] lg:w-[90px] lg:h-[90px] xl:w-[110px] xl:h-[110px] object-cover rounded-full'
+                                        />
+                                        <div className='my-5 text-[13px] lg:text-base xl:text-xl text-center'>
+                                            <p className='font-semibold text-base md:text-lg lg:text-xl xl:text-2xl'>{item.name}</p>
+                                            {item.position}
+                                        </div>
+                                    </div>
+                                    <span className='text-base font-semibold my-4 xl:my-6 md:text-lg lg:text-xl'>
+                                        {item.qualifications}
+                                    </span>
+                                    <p className='my-3 text-[13px] lg:text-base xl:text-xl'>
+                                        {item.experience}
+                                    </p>
+                                </div>
+                            </div>)
+                        }
+                    </Slider>
+                </div>
+
+                <Gallery />
+
             </div>
         </div>
     )
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,77 @@
 import React from 'react'
+import video from '../assets/School_Infra.mp4'
 
 export default function Gallery() {
-    return (
-        <div>
 
+
+    let photoData = [
+        {
+            url: 'https://www.kingschester.co.uk/wp-content/uploads/2019/07/IMG_1258.jpg',
+            caption: 'Sports Day 2023',
+            description: 'Students participating in the annual sports day event.'
+        },
+        {
+            url: 'https://th.bing.com/th/id/R.53f95536e820cfa21f61f730da02f5b4?rik=grntTH9DTmadsQ&riu=http%3a%2f%2fwww.abington.k12.pa.us%2fmedia%2fposts%2f2017%2f03%2fDSC05494.jpg&ehk=KQkrWUPfwuKTks35nm0upxSBGoFYTtTI2dk335YGkPc%3d&risl=1&pid=ImgRaw&r=0',
+            caption: 'Science Fair 2023',
+            description: 'Innovative science projects presented by students.'
+        },
+        {
+            url: 'https://education.jnto.go.jp/cms/wp-content/uploads/2022/02/culture-festival-01.jpg',
+            caption: 'Cultural Fest 2023',
+            description: 'A vibrant celebration of diverse cultures.'
+        },
+        {
+            url: 'https://www.chicagogallerynews.com/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaHNPIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e3d5e28f02ed2d50be19a9075e2e005c791333bd/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9MY21WemFYcGxTU0lPTVRJd01IZzNNREErQmpvR1JWUT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--879673d8e0691dd8d0132388f11119abd2f4098e/high-school-show.jpg',
+            caption: 'Art Exhibition 2023',
+            description: 'A display of stunning artworks created by students.'
+        },
+        {
+            url: 'https://th.bing.com/th/id/OIP.gNsxA3rGhXFQHvvlPBUgKwHaE8?rs=1&pid=ImgDetMain',
+            caption: 'Field Trip 2023',
+            description: 'An educational trip to the historical museum.'
+        },
+        {
+            url: 'https://cdngeneral.rentcafe.com/dmslivecafe/UploadedImages/03479a27-938f-4d75-8635-f63025ef11b3.jpg',
+            caption: 'Graduation Day 2023',
+            description: 'Celebrating the achievements of our graduates.'
+        },
+        {
+            url: 'https://www.millhill.org.uk/wp-content/uploads/0006.jpg',
+            caption: 'Music Concert 2023',
+            description: 'Students showcasing their musical talents.'
+        },
+        {
+            url: 'https://th.bing.com/th/id/OIP.O_ByzRhO-BEMhk-W1edF5wHaFH?rs=1&pid=ImgDetMain',
+            caption: 'Drama Performance 2023',
+            description: 'An enthralling drama performance by the students.'
+        },
+    ]
+
+    return (
+        <div className='text-center'>
+            <h1 className='text-4xl lg:text-5xl xl:text-7xl font-semibold'>School Gallery</h1>
+            <p className='lg:text-lg my-6'>Explore our school events, activities, and infrastructure through our photo and video gallery.</p>
+            <div className='relative'>
+                <video src={video} autoPlay loop muted className='w-full max-h-screen'></video>
+                <h2 className='text-lg md:text-3xl lg:text-5xl font-semibold absolute top-[40%] md:top-[45%] right-[10%]'>School Infrastructure</h2>
+            </div>
+            <h1 className='mt-10 mb-6 text-left text-2xl lg:text-4xl xl:text-6xl font-semibold'>School Events</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+                {photoData.map((photo, index) => (
+                    <div key={index} className="bg-blue-600 hover:opacity-90 overflow-hidden rounded-xl">
+                        <img
+                            src={photo.url}
+                            alt={photo.caption}
+                            className="w-full h-auto rounded-t-xl hover:scale-105 duration-300 cursor-pointer"
+
+                        />
+                        <div className="p-2 sm:py-4">
+                            <h3 className="font-semibold lg:text-lg">{photo.caption}</h3>
+                            <p className="text-sm">{photo.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
